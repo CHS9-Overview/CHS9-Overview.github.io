f7a828eaf5e1c8119c20458920ca5d58bb1abe10
CHS9-Overview <sxv1090@gmail.com>
2025-01-11 23:17:34 +0000

    normal update

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ function toggle(sec) {
     }
     else{
       document.getElementById(i.toString()).style.display = "block";
+      
+      num = i.toString()+"0";
+      const headers = document.getElementsByTagName('h2');
+      for (let i = 0; i < headers.length; i++) {
+        if(headers[i].textContent != 'More Options' || headers[i].textContent != 'What&#39; Happening?')
+        headers[i].classList.add("highlight");
+      }
     }
   }
 }
