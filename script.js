718e11ed302d87cf7e632ca2fa818df316e01fcf
CHS9-Overview <sxv1090@gmail.com>
2025-01-24 20:35:06 +0000

    slkdfj

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ function toggle(sec) {
     }
   }
 }
+function performSearch(){
+  var search = document.getElementById("searchQuery").value;
+  search = search.toLowerCase();
+  if(search == ""){
+    alert("Please enter a search term");
+  }
+  else{
+    var docText = document.documentElement.textContent;
+    docText = docText.toLowerCase();
+    if(docText.includes(search)){
+      getSearchResults(search);
+    } else{
+      alert("No results found");
+    }
+  }
+}
+function getSearchResults(search){
+  const elements = document.getElementsByTagName('*');
+  for (let i = 0; i < elements.length; i++){
+    var text = elements[i].textContent;
+    text = text.toLowerCase();
+    if(text.includes(search)){
+      searchResults.push(text);
+    }
+  }
+  var results = document.getElementById("searchResults");
+  results.innerHTML = "";
+  
+}
